a5c0910b27560e82fa2b65b78c26b703fa1392c1
dev-null-loop <dev.null.loop@gmail.com>
2026-05-18 15:47:23 +0300

    Fix SERVICE CATALOG variable descriptions

--- private_application/variables.tf
+++ private_application/variables.tf
@@ -1,39 +1,39 @@
 variable "compartment_id" {
-  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment where you want to create the private application."
+  description = "(Required) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment where you want to create the private application."
   type        = string
 }
 
 variable "defined_tags" {
-  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`"
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. Example: `{\"foo-namespace.bar-key\": \"value\"}`"
   type        = map(string)
   default     = null
 }
 
 variable "display_name" {
-  description = "The name of the private application."
+  description = "(Required) (Updatable) The name of the private application."
   type        = string
 }
 
 variable "freeform_tags" {
-  description = "Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
+  description = "(Optional) (Updatable) Simple key-value pair that is applied without any predefined name, type or scope. Exists for cross-compatibility only. Example: `{\"bar-key\": \"value\"}`"
   type        = map(string)
   default     = {}
 }
 
 variable "logo_file_base64encoded" {
-  description = "Base64-encoded logo to use as the private application icon. Template icon file requirements: PNG format, 50 KB maximum, 130 x 130 pixels."
+  description = "(Optional) (Updatable) Base64-encoded logo to use as the private application icon. Template icon file requirements: PNG format, 50 KB maximum, 130 x 130 pixels."
   type        = string
   default     = null
 }
 
 variable "long_description" {
-  description = "A long description of the private application."
+  description = "(Optional) (Updatable) A long description of the private application."
   type        = string
   default     = null
 }
 
 variable "package_details" {
-  description = "A base object for creating a private application package."
+  description = "(Required) A base object for creating a private application package."
   type = object({
     package_type           = string
     version                = string
@@ -42,6 +42,6 @@ variable "package_details" {
 }
 
 variable "short_description" {
-  description = "A short description of the private application."
+  description = "(Required) (Updatable) A short description of the private application."
   type        = string
 }
--- service_catalog_association/variables.tf
+++ service_catalog_association/variables.tf
@@ -1,15 +1,15 @@
 variable "entity_id" {
-  description = "Identifier of the entity being associated with service catalog."
+  description = "(Required) Identifier of the entity being associated with service catalog."
   type        = string
 }
 
 variable "entity_type" {
-  description = "The type of the entity that is associated with the service catalog."
+  description = "(Optional) The type of the entity that is associated with the service catalog."
   type        = string
   default     = null
 }
 
 variable "service_catalog_id" {
-  description = "Identifier of the service catalog."
+  description = "(Required) Identifier of the service catalog."
   type        = string
 }
